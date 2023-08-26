5b3ea7cc4346b80bb5184780112ba014fc83a6b6
Botshelo Brandon Tidimalo <botshelo@innovateium.co.bw>
2023-08-26 15:33:12 +0200

    Format home page imports

--- lib/pages/home_page/home_page_model.dart
+++ lib/pages/home_page/home_page_model.dart
@@ -1,14 +1,8 @@
+import 'package:flutter/material.dart';
+
 import '/backend/api_requests/api_calls.dart';
 import '/components/loading_animation_widget.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/custom_functions.dart' as functions;
-import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 class HomePageModel extends FlutterFlowModel {
   ///  Local state fields for this page.
